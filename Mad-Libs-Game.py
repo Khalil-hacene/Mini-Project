d0fd5c68caa59c006729e3d52d9ad887084aca3c
Khalil Khaled Hacene <Khalilkhaledh@gmail.com>
2025-07-02 17:16:36 +0100

    Add files via upload

--- Mad-Libs-Game.py
+++ Mad-Libs-Game.py
@@ -1,10 +1,18 @@
 # Madlibls game 
-adj1 = input("enter an adjective (descrption):")
-noun1 = input("enter a noun (person,place,thing):")
-adj2 = input("enter an adjective (descrption):")
-verb1 = input("enter a verb ending with an 'ing':")
-adj3 = input("enter an adjective (descrption):")
+
+# The Words you tape will fill the blanks
+
+adj1 = input("Enter an adjective (descrption): ")
+noun1 = input("Enter a noun (person,place,thing): ")
+adj2 = input("Enter an adjective (descrption): ")
+verb1 = input("Enter a verb ending with an 'ing': ")
+adj3 = input("Enter an adjective (descrption): ")
+
+# What's gonna be printed
+
 print(f"Today I Went to a {adj1} zoo.")
 print(f"in an exhibit, i saw a {noun1}")
-print(f"f{noun1} was {adj2} and {verb1}")
+print(f"{noun1} was {adj2} and {verb1}")
 print(f"I was {adj3}!")
+
+# End of the Code
