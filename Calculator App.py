35a95cab51dae57d093b0864152985a6160913bb
Khalil Khaled Hacene <Khalilkhaledh@gmail.com>
2025-07-05 22:57:05 +0100

    Add files via upload

--- Calculator App.py
+++ Calculator App.py
@@ -1,24 +1,28 @@
 # Calculator program 
+running = True
+while running:
+    operator = input("Enter an operator ('+' '-' '*' '/' '**' '%'): ")
+    num1 = float(input("Enter the First number: "))
+    num2 = float(input("Enter the Second number: "))
 
-operator = input("Enter an operator ('+' '-' '*' '/' '**' '%'): ")
-num1 = float(input("Enter the First number: "))
-num2 = float(input("Enter the Second number: "))
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+        if num2 == 0:
+            print(f"You can't divide by {num2}")
+    elif operator == "**":
+        result = num1 ** num2
+    elif operator == "%":
+        result = num1 % num2
+    else:
+        print(f"You did put the wrong {operator} please select one of this ('+' '-' '*' '/' '**' '%')")
+    
+    print(round(result, 4))
 
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    result = num1 / num2
-    if num2 == 0:
-        print(f"You can't divide by {num2}")
-elif operator == "**":
-    result = num1 ** num2
-elif operator == "%":
-    result = num1 % num2
-else:
-    print(f"You did put the wrong {operator} please select one of this ('+' '-' '*' '/' '**' '%')")
-
-print(round(result, 4))
+    if input("if you wanna calcute again enter (y/n): ") == "n":
+        break
