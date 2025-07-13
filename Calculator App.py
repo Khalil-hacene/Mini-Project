bda99f7333de22a651c122b63e50218e64735dcc
Khalil Khaled Hacene <Khalilkhaledh@gmail.com>
2025-07-13 17:50:34 +0100

    Update Calculator App.py

--- Calculator App.py
+++ Calculator App.py
@@ -1,8 +1,12 @@
 # Calculator program 
 while True:
-    operator = input("Enter an operator ('+' '-' '*' '/' '**' '%'): ")
-    num1 = float(input("Enter the First number: "))
-    num2 = float(input("Enter the Second number: "))
+    operator = input("Enter an operator ('+' '-' '*' '/' '**' '%'): ")    
+    try:
+        num1 = float(input("Enter the First number: "))
+        num2 = float(input("Enter the Second number: "))
+    except:
+        print("Invalid input")
+        continue
 
     if operator == "+":
         result = num1 + num2
@@ -21,8 +25,9 @@ while True:
         result = num1 % num2
     else:
         print(f"You did put the wrong {operator} please select one of this ('+' '-' '*' '/' '**' '%')")
+        continue 
     
     print(round(result, 4))
 
-    if input("if you wanna calcute again enter (y/n): ") == "n":
+    if input("if you wanna calculate again enter (y/n): ") == "n":
         break
