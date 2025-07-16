52a5283cb0e6c6e7431a648e776272970b0bdeeb
Khalil Khaled Hacene <Khalilkhaledh@gmail.com>
2025-07-16 14:25:55 +0100

    Update Mad-Libs-Game.py

--- Mad-Libs-Game.py
+++ Mad-Libs-Game.py
@@ -4,14 +4,14 @@
 
 adj1 = input("Enter an adjective (descrption): ")
 noun1 = input("Enter a noun (person,place,thing): ")
-adj2 = input("Enter an adjective (descrption): ")
+adj2 = input("Enter an adjective (description): ")
 verb1 = input("Enter a verb ending with an 'ing': ")
-adj3 = input("Enter an adjective (descrption): ")
+adj3 = input("Enter an adjective (description): ")
 
 # What's gonna be printed
 
 print(f"Today I Went to a {adj1} zoo.")
-print(f"in an exhibit, i saw a {noun1}")
+print(f"In an exhibit, i saw a {noun1}")
 print(f"{noun1} was {adj2} and {verb1}")
 print(f"I was {adj3}!")
 
